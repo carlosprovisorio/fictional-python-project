067ad5d0e9611205d2296113212d24e71c0a9f93
Carlos <carlosprovisorio@icloud.com>
2024-07-17 16:02:50 -0400

    Data Structure

---
+++ data_structure.py
@@ -0,0 +1,19 @@
+# Lists
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)
+print(fruits[1])  # banana
+
+# Tuples
+point = (10, 20)
+print(point[0])  # 10
+
+# Sets
+unique_numbers = {1, 2, 3, 4}
+unique_numbers.add(5)
+print(unique_numbers)
+
+# Dictionaries
+person = {"name": "Carlos", "age": 39}
+person["age"] = 31
+print(person["name"])
