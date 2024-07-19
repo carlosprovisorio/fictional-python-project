0f9a5099288f739c6aa2ed846fd60be835c8d24f
Carlos <carlosprovisorio@icloud.com>
2024-07-19 12:58:05 -0400

    COnditional statement

--- app.py
+++ app.py
@@ -75,10 +75,23 @@
 # bool("") False
 # bool("False") True
 
-print(10 > 3)
-print(10 >= 3)
-print(10 < 3)
-print(10 <= 3)
-print(10 == 10)
-print(10 == "10")
-print(10 != "10")
+# print(10 > 3)
+# print(10 >= 3)
+# print(10 < 3)
+# print(10 <= 3)
+# print(10 == 10)
+# print(10 == "10")
+# print(10 != "10")
+# "bag" > "apple"
+# "bag" == "BAG"
+
+temperature = 15
+
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20:
+    print("It's nice")
+else:
+    print("It's cold")
+print("Done")
