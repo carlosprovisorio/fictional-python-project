6a96f9bff70f4e2d393a36f3174c19e220ef29d0
Carlos <carlosprovisorio@icloud.com>
2024-07-18 20:51:21 -0400

    Primitive types

---
+++ app.py
@@ -0,0 +1,14 @@
+# students_count = 1000  # integer
+# rating = 4.99  # float
+# is_published = True  # boolean
+# course_name = "Python Programming"
+# print(students_count)
+
+
+# course = "Python Programming"
+# print(len(course))
+# print(course[-1])
+# print(course[0:3])
+# print(course[0:])
+# print(course[:3])
+# print(course[:])
