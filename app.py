6353d23c9916e6e2672a5880a21cfea112796960
Carlos <carlosprovisorio@icloud.com>
2024-07-18 21:01:38 -0400

    String methods and Escape sequence

--- app.py
+++ app.py
@@ -12,3 +12,19 @@
 # print(course[0:])
 # print(course[:3])
 # print(course[:])
+
+
+# Scape sequence in Python
+# \"
+# \'
+# \\
+# \n
+
+# course = 'Python "Programming'
+# print(course)
+
+first = "Carlos"
+last = "Ferreira"
+# full = first + " " + last     # Ugly
+full = f"{first} {last}"  # better version
+print(full)
