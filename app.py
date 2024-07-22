f923aa36b4327098718885b3e24e3b70201fbbf5
Carlos <carlosprovisorio@icloud.com>
2024-07-22 16:47:56 -0400

    Latest changes quiz

--- app.py
+++ app.py
@@ -74,24 +74,3 @@
 # bool(5) True
 # bool("") False
 # bool("False") True
-
-# print(10 > 3)
-# print(10 >= 3)
-# print(10 < 3)
-# print(10 <= 3)
-# print(10 == 10)
-# print(10 == "10")
-# print(10 != "10")
-# "bag" > "apple"
-# "bag" == "BAG"
-
-temperature = 15
-
-if temperature > 30:
-    print("It's warm")
-    print("Drink water")
-elif temperature > 20:
-    print("It's nice")
-else:
-    print("It's cold")
-print("Done")
